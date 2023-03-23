ad16ff186ee427698f518ec918b15b491abcb825
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-03-23 23:57:32 +0500

    admin login and regestration completed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,4 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::get('/redirect',[HomeController::class,'redirect'])->name('redirect');
